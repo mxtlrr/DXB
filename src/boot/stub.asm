e8f6ae37d79920a9477deaadcc86ea04cdeccd24
mxtlrr <ruson0914@gmail.com>
2024-09-14 18:03:02 -0500

    grunt: check for 80386 CPUs

--- src/boot/stub.asm
+++ src/boot/stub.asm
@@ -12,16 +12,57 @@ mov cx, 0x00002
 mov dh, 0x00
 int 0x13
 jc .Failure
-jnc 0x7e00
+
+;; Check if we're on 386.
+;; Won't support 286 for now.
+push sp
+pop ax
+cmp ax, sp
+jne .No386    ;; 286.
+
+;; MSW bits 15->4 are clear?
+smsw ax
+cmp ax, 0x0fff0
+jae .No386   ;; is AX >= 0x0FFF0?
+
+;; 386 is enabled!!
+jmp 0x7e00
+
+.No386:
+  mov bx, string_bad286
+  call printf
 
 ;; Returned? Quit.
 hcf
 jmp $
 
 .Failure:
-  mov ax, 0x0e5a
-  int 0x10
+  mov bx, string_nodisk
+  call printf
   hcf
+
+
+printf:
+  push ax
+  push bx
+
+  mov ah, 0x0e
+  .Loop:
+    cmp [bx], byte 0
+    je .Exit
+
+    mov al, [bx]
+    int 0x10
+
+    inc bx
+    jmp .Loop
+  .Exit:
+    pop bx
+    pop ax
+  ret
+
+string_nodisk: db `No dice! Can't read from 7e00h.`, 0
+string_bad286: db `Sorry, DXB doesn't support the 80286.`, 0
 
 times 510-($-$$) db 0
 dw 0xaa55
--- src/dxb.c
+++ src/dxb.c
@@ -3,7 +3,10 @@
 // what we want to.
 __asm__("jmp $0x0000, $_dxb_main\n");
 
-void __int10h(unsigned short ax){
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+
+void __int10h(uint16_t ax){
   __asm__("mov %0, %%ax\nint $0x10" ::"r"(ax) :"%ax");
 }
 
@@ -15,9 +18,9 @@ void puts(char* s){
 }
 
 // Write a byte to output
-void putb(unsigned char byte){
-  unsigned char hi = byte >> 4;
-  unsigned char lo = byte & 0x0f;
+void putb(uint8_t byte){
+  uint8_t hi = byte >> 4;
+  uint8_t lo = byte & 0x0f;
 
   if(hi > 9){ putc(((hi-10) + 0x41)); }
   else {      putc((hi + 0x30));      }
@@ -26,6 +29,14 @@ void putb(unsigned char byte){
   else {      putc((lo + 0x30));      }
   
 }
+
+
+// Ok, let's do some fun stuff!
+// Sort of a port of a DOS program. You can see it
+// here: https://web.archive.org/web/20171130135644/http://www.mess.org/_media/dumping/dumpat.zip
+#define BIOS_START_386 0xfff0000
+#define BIOS_SIZE      0xFFFF    // 65kb
+// Rarely mapped to different locations, but you never know!:)
 
 void _dxb_main(void){
   putb(0xfa);
