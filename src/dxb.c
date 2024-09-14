e86fdb89ac37829b86d72d12f3212b9c3721dd0f
mxtlrr <ruson0914@gmail.com>
2024-09-14 13:21:49 -0500

    fix: fix random code execution

--- src/dxb.c
+++ src/dxb.c
@@ -1,3 +1,9 @@
+// Long jump to the label, avoiding any weird code generation.
+// If we don't, we'll execute random code and go off past
+// what we want to.
+__asm__("jmp $0x0000, $_dxb_main\n");
+
+
 void _dxb_main(void){
   for(;;);
 }
